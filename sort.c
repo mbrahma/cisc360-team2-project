8296d44ab681808e20f5ecfdf99fa94e7a672cc3
mark <mark@mark-Satellite-L855>
2015-04-30 19:07:45 -0400

    radix sort and quicksort implemented

--- sort.c
+++ sort.c
@@ -14,6 +14,10 @@
  *  Sort the inpwutted integers, using merge-sort algorithm.
  **/
 // http://www.cs.cityu.edu.hk/~lwang/ccs4335/mergesort.c
+// Quicksort:
+// http://www.comp.dit.ie/rlawlor/Alg_DS/sorting/quickSort.c
+// RadixSort:
+// http://austingwalters.com/radix-sort-in-c/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -95,12 +99,109 @@ merge_sort(int *A, int n)
   free(A2);
 }
 
+int partition( int a[], int l, int r) {
+   int pivot, i, j, t;
+   pivot = a[l];
+   i = l; j = r+1;
+
+   while( 1)
+   {
+   	do ++i; while( a[i] <= pivot && i <= r );
+   	do --j; while( a[j] > pivot );
+   	if( i >= j ) break;
+   	t = a[i]; a[i] = a[j]; a[j] = t;
+   }
+   t = a[l]; a[l] = a[j]; a[j] = t;
+   return j;
+}
+
+void quickSort( int a[], int l, int r)
+{
+   int j;
+
+   if( l < r )
+   {
+   	// divide and conquer
+        j = partition( a, l, r);
+       quickSort( a, l, j-1);
+       quickSort( a, j+1, r);
+   }
+
+}
+void printArray(int * array, int size){
+
+  int i;
+  printf("[ ");
+  for (i = 0; i < size; i++)
+    printf("%d ", array[i]);
+  printf("]\n");
+}
+
+int findLargestNum(int * array, int size){
+
+  int i;
+  int largestNum = -1;
+
+  for(i = 0; i < size; i++){
+    if(array[i] > largestNum)
+      largestNum = array[i];
+  }
+
+  return largestNum;
+}
+
+// Radix Sort
+void radixSort(int * array, int size){
+
+  printf("\n\nRunning Radix Sort on Unsorted List!\n\n");
+
+  // Base 10 is used
+  int i;
+  int semiSorted[size];
+  int significantDigit = 1;
+  int largestNum = findLargestNum(array, size);
+
+  // Loop until we reach the largest significant digit
+  while (largestNum / significantDigit > 0){
+
+    printf("\tSorting: %d's place ", significantDigit);
+    printArray(array, size);
+
+    int bucket[10] = { 0 };
+
+    // Counts the number of "keys" or digits that will go into each bucket
+    for (i = 0; i < size; i++)
+      bucket[(array[i] / significantDigit) % 10]++;
+
+    /**
+     * Add the count of the previous buckets,
+     * Acquires the indexes after the end of each bucket location in the array
+		 * Works similar to the count sort algorithm
+     **/
+    for (i = 1; i < 10; i++)
+      bucket[i] += bucket[i - 1];
+
+    // Use the bucket to fill a "semiSorted" array
+    for (i = size - 1; i >= 0; i--)
+      semiSorted[--bucket[(array[i] / significantDigit) % 10]] = array[i];
+
+
+    for (i = 0; i < size; i++)
+      array[i] = semiSorted[i];
+
+    // Move to next significant digit
+    significantDigit *= 10;
+
+    printf("\n\tBucket: ");
+    printArray(bucket, 10);
+  }
+}
+
 /**
  *  main()
  *  The main function for input and output
  **/
-int
-main(int argv, char** args)
+int main(int argv, char** args)
 {
   int i, n;
   int *A;
@@ -134,9 +235,34 @@ main(int argv, char** args)
   printf("\n");
   free(A);
 
-
 //  printf("Press ENTER to return.\n");
 //  c = _getch();
+	int a[] = { 7, 12, 1, -2, 0, 15, 4, 11, 9};
 
+	printf("\n\nUnsorted array is:  ");
+	for(i = 0; i < 9; ++i)
+		printf(" %d ", a[i]);
+
+	quickSort( a, 0, 8);
+
+	printf("\n\nSorted array is:  ");
+	for(i = 0; i < 9; ++i)
+		printf(" %d ", a[i]);
+	  printf("\n\nRunning Radix Sort Example in C!\n");
+	  printf("----------------------------------\n");
+
+	  int size = 12;
+	  int list[] = {10, 2, 303, 4021, 293, 1, 0, 429, 480, 92, 2999, 14};
+
+	  printf("\nUnsorted List: ");
+	  printArray(&list[0], size);
+
+	  radixSort(&list[0], size);
+
+	  printf("\nSorted List:");
+	  printArray(&list[0], size);
+	  printf("\n");
+
+	  return 0;
 }
 
